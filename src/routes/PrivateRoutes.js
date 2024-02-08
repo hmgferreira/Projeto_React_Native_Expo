2d06f6ad8c45e6bbbc86607dba8397d5313920f7
WAGNER <diretoria@iwtraining.com.br>
2024-02-07 21:50:05 -0300

    Ajustes

--- src/routes/PrivateRoutes.js
+++ src/routes/PrivateRoutes.js
@@ -3,7 +3,7 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import HomeScreen from "../screens/HomeScreen";
 import CameraScreen from "../screens/CameraScreen";
-import MapaScreen from "../screens/MapaScreen";
+
 
 // const Stack = createNativeStackNavigator();
 const Stack = createDrawerNavigator();
@@ -12,10 +12,10 @@ export default function PrivateRoutes() {
     return(
         <NavigationContainer>
              <Stack.Navigator initialRouteName="Home">
-                <Stack.Screen name="Home" component={HomeScreen} options={{ headerShown: true }} />
-                <Stack.Screen name="Contatos" component={HomeScreen} options={{ headerShown: true }} />
-                <Stack.Screen name="Camera" component={CameraScreen} options={{ headerShown: true }} />
-                <Stack.Screen name="Mapa" component={MapaScreen} options={{ headerShown: true }} />
+                <Stack.Screen name="Home" component={HomeScreen} options={{ headerShown: false }} />
+                <Stack.Screen name="Contatos" component={HomeScreen} options={{ headerShown: false }} />
+                {/* <Stack.Screen name="Camera" component={CameraScreen} options={{ headerShown: true }} /> */}
+                
              </Stack.Navigator>
         </NavigationContainer>
     );
--- src/screens/HomeScreen/index.js
+++ src/screens/HomeScreen/index.js
@@ -1,10 +1,70 @@
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Button, TouchableHighlight, ScrollView, FlatList } from 'react-native';
+import { Feather } from '@expo/vector-icons';
+import { TextH1, TextH2, Paragraf, TextH3 } from '../../components/Texts';
+import Cards from '../../components/Cards';
+import Banners from '../../components/Banners';
 
-export default function HomeScreen() {
+export default function HomeScreen(props) {
+
+    function renderItem(dados) {
+        return (
+            <View style={{ flexDirection: 'row', alignItems: 'center', marginTop: 10 }}>
+                <Cards />
+                <View>
+                    <TextH3>Produto</TextH3>
+                    <Text>{dados.item.id} - {dados.item.nome}</Text>
+                </View>
+                
+                
+            </View>
+        )
+    }
  
     return (
         <View style={styles.container}>
-           <Text>HOMESCREEN</Text>            
+            <View style={styles.header}>
+                <View>
+                    <TouchableHighlight onPress={() => props.navigation.toggleDrawer()} >
+                        <Feather name="menu" size={32} color="black" />
+                    </TouchableHighlight> 
+                </View>
+                <View>
+                    B
+                </View>    
+            </View>
+
+            <View>
+                <TextH1>Olá, jovem</TextH1>
+                <Paragraf>Seja bem-vindo!</Paragraf>
+            </View>       
+            
+            <View style={{ height: 90 }}>
+                <ScrollView horizontal style={{ marginTop: 10 }}>
+                        <Cards />
+                        <Cards />
+                        <Cards />
+                        <Cards />
+                        <Cards />
+                        <Cards />
+                        <Cards />
+                        <Cards />
+                </ScrollView>
+            </View>
+            <Banners />
+            <Banners />
+            <Banners />
+
+            <View>
+                <FlatList 
+                    data={[
+                        { id: 1, nome: 'Ana' },
+                        { id: 2, nome: 'Maria' },
+                        { id: 3, nome: 'Eduardo' },
+                    ]}
+                    renderItem={renderItem}
+                />
+            </View>
+                    
         </View>
     )
 }
@@ -12,6 +72,11 @@ export default function HomeScreen() {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        
+        padding: 10
+    },
+    header: {
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'space-between'
     }
 });
